ca5fd159f50644f4a8d3ac0d2ca5e4053725083f
ThalitaC <thalitaceci@gmail.com>
2021-02-24 19:18:23 -0300

    finaliza exercicios do conteudo

--- bloco_4/dia_1/exercicio3.js
+++ bloco_4/dia_1/exercicio3.js
@@ -7,3 +7,22 @@ if (nota > 80) {
 } else {
   console.log("Você foi reprovada(o)");
 }
+
+let estado = "aprovada"
+
+switch(estado) {
+  case "aprovada":
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+  
+  case "lista":
+    console.log("Você está na nossa lista de espera");
+    break;
+
+  case "reprovada":
+    console.log("Você foi reprovada(o)");
+    break;
+
+  default:
+    console.log("Não se aplica");
+}
