1c58a711a1fe9a6eac6c23e0979e7068f5f910e3
ThalitaC <thalitaceci@gmail.com>
2021-02-24 16:12:51 -0300

    seugunda parte do exercicio

---
+++ bloco_4/dia_1/exercicio2.js
@@ -0,0 +1,10 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+var base = "5";
+var altura = "8";
