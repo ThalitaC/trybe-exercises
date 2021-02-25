91ef0c159c15ec2349893e55389d75181db0ec19
ThalitaC <thalitaceci@gmail.com>
2021-02-25 18:24:18 -0300

    a

--- bloco_4/dia_1/Exercicio.js
+++ bloco_4/dia_1/Exercicio.js
@@ -1,19 +1,21 @@
-//exercicio1
-let a = 5;
-let b = 10;
+// //exercicio1
+// let a = 5;
+// let b = 10;
 
-let soma = a  + b;
-let menos = a - b;
-let mult = a * b;
-let divide = b / a;
-let resto = b %a;
+// let soma = a  + b;
+// let menos = a - b;
+// let mult = a * b;
+// let divide = b / a;
+// let resto = b %a;
 
-//exercicio 2
-let c = 25;
-let d = 10;
+// //exercicio 2
+// let c = 25;
+// let d = 10;
 
-if (c > d) {
-  console.log("'c' é maior que 'd");
-} else {
-  console.log("'d é maior que 'c")
-}
+// if (c > d) {
+//   console.log("'c' é maior que 'd");
+// } else {
+//   console.log("'d é maior que 'c")
+// }
+
+
