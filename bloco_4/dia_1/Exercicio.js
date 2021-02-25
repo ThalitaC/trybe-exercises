1014fd86180432c282b6bf840c2b622a9ebbb264
ThalitaC <thalitaceci@gmail.com>
2021-02-25 15:40:31 -0300

    exercicio 2

--- bloco_4/dia_1/Exercicio.js
+++ bloco_4/dia_1/Exercicio.js
@@ -1,3 +1,4 @@
+//exercicio1
 let a = 5;
 let b = 10;
 
@@ -7,4 +8,12 @@ let mult = a * b;
 let divide = b / a;
 let resto = b %a;
 
-console.log(resto);
+//exercicio 2
+let c = 25;
+let d = 10;
+
+if (c > d) {
+  console.log("'c' é maior que 'd");
+} else {
+  console.log("'d é maior que 'c")
+}
