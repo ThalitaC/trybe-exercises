ff4f6248b736082fe0aa0bf6427f23ac4d54e974
ThalitaC <thalitaceci@gmail.com>
2021-02-26 16:06:41 -0300

    ate exercicio 5

--- bloco_4/dia_1/Exercicio.js
+++ bloco_4/dia_1/Exercicio.js
@@ -18,4 +18,44 @@
 //   console.log("'d é maior que 'c")
 // }
 
+// //exercicio 3
+// let num1 = 25;
+// let num2 = 35;
+// let num3 = 50;
+
+// if (num1 > num2 && num1 >num3) {
+//   console.log("o maior número é " + num1);
+// } else if (num2 > num1 && num2 > num3) {
+//   console.log("o maior número é " + num2);
+// } else {
+//   console.log("o maior número é " + num3);
+// }
+
+// //exercicio 4
+// let number = 0;
+
+// if (number > 0) {
+//   console.log("positive");
+// } else if (number < 0) {
+//   console.log ("negative");
+// } else {
+//   console.log ("zero")
+// }
+
+//exercicio 5
+let angle1 = -50;
+let angle2 = 50;
+let angle3 = 50;
+
+let positiveAngle = angle1 > 0 && angle2 > 0 && angle3 > 0;
+
+if (positiveAngle){
+  if ((angle1 + angle2 + angle3) === 180) {
+    console.log(true);
+  } else if ((angle1 + angle2 + angle3) != 180) {
+    console.log(false)
+  }
+} else {
+  console.log("erro: todos os angulos precisam ser numeros positivos")
+}
 
