1e246f4cb48f2f1afbb3a2942679fce462157f90
ThalitaC <thalitaceci@gmail.com>
2021-03-02 11:37:27 -0300

    corrige exercicio 2

--- bloco_4/dia_4/part_2/exercicio2.js
+++ bloco_4/dia_4/part_2/exercicio2.js
@@ -3,10 +3,10 @@ let maiorIndex = 0;
 
   for (let index in num) {
     if (num[maiorIndex] < num[index]){
-      maior = index;
+      maiorIndex = index;
     }    
   }
-  return maior;
+  return maiorIndex;
 }
 
-console.log(maior([1, 2, 0, 10, 8]));
+console.log(maior([1, 2, 0, 10, 8, 20, 5]));
