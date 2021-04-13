fc58fcd5d157eaab56bfe48466c282470389baa6
ThalitaC <thalitaceci@gmail.com>
2021-04-13 18:05:12 -0300

    exercicios 3, 4, 5, 6

--- bloco_9/dia_1/exercicios.js
+++ bloco_9/dia_1/exercicios.js
@@ -11,3 +11,64 @@ const getPlanet = () => {
   };
   
   getPlanet();
+
+// exercicio 4
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => console.log(`Mars temperature is: ${currentTemperature} degree Celsius`), messageDelay());
+};
+
+sendMarsTemperature();
+
+// exercicio 5
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const sendMarsTemperature = (onSuccess) => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => onSuccess(currentTemperature), messageDelay());
+};
+
+sendMarsTemperature(temperatureInFahrenheit);
+sendMarsTemperature(greet);
+
+// exercicio 6
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
+const sendMarsTemperature = (onSuccess, onError) => {
+    const currentTemperature = getMarsTemperature();
+    const messageSuccessfullySent = Math.random() <= 0.6;
+    setTimeout(() => {
+        if(messageSuccessfullySent) onSuccess(currentTemperature)
+        else onError('Robot is busy');
+    }, messageDelay());
+  }
+
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+sendMarsTemperature(greet, handleError);
